3d5f96263ac4b47f9e05baa3662254bb827ac184
mz121star <mz121star@gmail.com>
2014-09-04 12:49:44 +0800

    a

--- js/ieextend.js
+++ js/ieextend.js
@@ -38,7 +38,7 @@ define([], function () {
 		   if(index==9){
                $('.bg1111_1').css("filter","progid:DXImageTransform.Microsoft.Alpha(opacity=100)")
 				$('.bg1111').animate({opacity:0},6000);
-				$('.bg111').animate({left:'40%',opacity:1},1000);
+			//	$('.bg111').animate({left:'40%',opacity:1},1000);
 		   }
 		   if(index==10){
 				//$('.bg121').animate({left:'200px'},1000);
